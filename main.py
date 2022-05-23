7f539e78c60c82eb164a50df5c50c78e87496067
theLahcim <michal.mazur32@gmail.com>
2022-05-23 12:21:39 +0200

    crawl + scrap = main

---
+++ main.py
@@ -0,0 +1,6 @@
+from crawler import Crawler
+from scraper import Scraper
+
+if __name__ == '__main__':
+    Crawler(urls=['https://www.taniaksiazka.pl/']).run()
+    Scraper(query='SELECT url FROM ksiazki WHERE scrapped = False').scrap_data()
